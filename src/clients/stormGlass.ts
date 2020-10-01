fd6697638954934d26f3c738b6cc58c1480c7e0e
GBShadow <gbs.shadow@gmail.com>
2020-10-01 13:39:46 -0300

    Storm Glass client Connect

--- src/clients/stormGlass.ts
+++ src/clients/stormGlass.ts
@@ -1,5 +1,6 @@
 import { InternalError } from '@src/utils/errors/internal-error'
-import { AxiosStatic } from 'axios'
+import config, { IConfig } from 'config'
+import * as HTTPUtil from '@src/utils/request'
 
 export interface StormGlassSource {
   [key: string]: number
@@ -41,10 +42,13 @@ export class ClientRequestError extends InternalError {
 
 export class ClientResponseError extends InternalError {
   constructor(message: string) {
-    const internalMessage = 'Unexpected error return by the StormGlass service'
+    const internalMessage =
+      'Unexpected error returned by the StormGlass service'
     super(`${internalMessage}: ${message}`)
   }
 }
+
+const stormGlassResourceConfig: IConfig = config.get('App.resources.StormGlass')
 
 export class StormGlass {
   readonly stormGlassAPIParams =
@@ -52,23 +56,25 @@ export class StormGlass {
 
   readonly stormGlassAPISource = 'noaa'
 
-  constructor(protected request: AxiosStatic) {}
+  constructor(protected request = new HTTPUtil.Request()) {}
 
   public async fetchPoints(lat: number, lng: number): Promise<ForecastPoint[]> {
     try {
       const response = await this.request.get<StormGlassForecastResponse>(
-        `https://api.stormglass.io/v2/weather/point?params=${this.stormGlassAPIParams}&source=${this.stormGlassAPISource}&lat=${lat}&lng=${lng}`,
+        `${stormGlassResourceConfig.get('apiUrl')}/weather/point?params=${
+          this.stormGlassAPIParams
+        }&source=${this.stormGlassAPISource}&lat=${lat}&lng=${lng}`,
         {
           headers: {
-            Authorization: 'fake-token'
+            Authorization: `${stormGlassResourceConfig.get('apiToken')}`
           }
         }
       )
       return this.normalizedResponse(response.data)
     } catch (err) {
-      if (err.response && err.response.status) {
+      if (HTTPUtil.Request.isRequestError(err)) {
         throw new ClientResponseError(
-          `Error ${JSON.stringify(err.response.data)} Code: ${
+          `Error: ${JSON.stringify(err.response.data)} Code: ${
             err.response.status
           } `
         )
